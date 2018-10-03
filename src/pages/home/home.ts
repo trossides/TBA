38c1c806fc27c942e5ce92f51a7ed4be2edba419
trossides <theorossides@gmail.com>
2018-10-03 22:10:00 +0930

    Getting user input with alert components

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public desires = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertController: AlertController) {
+
+  }
+
+  openDesireAlert(){
+    let addDesireAlert = this.alertController.create({
+      title: "Add A Desire",
+      message: "Enter Your Desire",
+      inputs: [
+        {
+          type: "text",
+          name: "addDesireInput"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel"
+        },
+        {
+          text: "Add Desire",
+          handler: (inputData)=> {
+            let desireText;
+            desireText = inputData.addDesireInput;
+            this.desires.push(desireText);
+          }
+        }
+      ]
+    });
+    addDesireAlert.present();
 
   }
 
